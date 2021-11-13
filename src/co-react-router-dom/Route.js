985ff90da1d4e9c67482426f855ab84b621e2a0a
unknown <416212316@qq.com>
2021-11-14 00:23:35 +0800

    dynamic route

--- src/co-react-router-dom/Route.js
+++ src/co-react-router-dom/Route.js
@@ -1,12 +1,17 @@
+import React from 'react'
 import { Component } from 'react'
+import RouterContext from './RouterContext'
+import matchPath from './matchPath'
 
 class Route extends Component {
     render() {
-        return (
-            <div>
-                <h3>Route</h3>
-            </div>
-        )
+        return <RouterContext.Consumer>{context => {
+            const {path, component} = this.props
+            const {location} = context
+            // const match = location.pathname === path
+            const match = matchPath(location.pathname, this.props)
+            return match ? React.createElement(component) : null
+        }}</RouterContext.Consumer>
     }
 }
 
--- src/co-react-router-dom/Router.js
+++ src/co-react-router-dom/Router.js
@@ -2,9 +2,16 @@ import { Component } from 'react'
 import RouterContext from './RouterContext'
 
 class Router extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {location: props.history.location}
+        props.history.listen((location) => {
+            this.setState({location})
+        })
+    }
     render() {
         return (
-            <RouterContext.Provider value={{history: this.props.history}}>
+            <RouterContext.Provider value={{history: this.props.history, location: this.state.location}}>
                 {this.props.children}
             </RouterContext.Provider>
         )
